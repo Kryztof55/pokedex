ba65aeb5fee0e550ce93da9d0e6e86d5b753d163
Kryztof55 <c.farango.dg@gmail.com>
2023-03-02 13:21:06 -0600

    chore: test commit to link codesanbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// test to link with codesanbox
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
